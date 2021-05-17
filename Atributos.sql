10eb7476de401a6e5a18341dafad50a1f3cefeea
Piter <pmedelcorrea@gmail.com>
2021-05-17 01:45:06 -0400

    alo

--- Atributos.sql
+++ Atributos.sql
@@ -3,4 +3,13 @@
 -- 1  NOT NULL   --> Evita que se ingresen valores nulos a una columa especifica
 -- 2  UNIQUE     --> Evita que se ingresen valores duplicados en una columna o conjunto de columnas
 -- 3  UNSIGNED   --> Evita que se ingresen numeros negativos a una columna especifica
--- 4  
+-- 4  DEFAULT
+
+
+
+
+
+-- EJEMPLOS
+
+--1
+gio
