4b5754e5fb6152c15937ecfe40a723d0e363ee0c
Piter <pmedelcorrea@gmail.com>
2021-05-16 23:12:48 -0400

    prueba git

--- Atributos.sql
+++ Atributos.sql
@@ -1,3 +1,5 @@
+-- Atributos y restricciones
+
 -- 1  NOT NULL   --> Evita que se ingresen valores nulos a una columa especifica
 -- 2  UNIQUE     --> Evita que se ingresen valores duplicados en una columna o conjunto de columnas
 -- 3  UNSIGNED   --> Evita que se ingresen numeros negativos a una columna especifica
